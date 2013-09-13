7eff8782223fc032babee45ab35c58af709e5317
Shuai Zhang <zhang_s@worksap.co.jp>
2013-09-13 12:35:19 +0800

    add event handler for awesome tree

--- src/awesome-tree.js
+++ src/awesome-tree.js
@@ -1,23 +1,66 @@
 (function($) {
 	"use strict";
 
-	$.awesomeTree = {
+	//the default settings for awesome tree.
+	var settings = {
+		cursorAt: {left: 10, top: -40}
+	};
 
-		//the default parameter option for awesome tree.
-		defaults: {
+	var debug = function(obj) {
+		if(window.console && window.console.log) {
+			window.console.log(obj);
+		}
+	};
 
-		};
+	var handleDraggableStart = function(event, ui) {
+		debug("drag event start...");
+	};
 
+	var handleDraggableStop = function(event, ui) {
+		debug("drag event stop...");
+	};
+
+	var handleDroppableDrop = function(event, ui) {
+		debug("drop event drop...");
+	};
+
+	var handleDroppableOver = function(event, ui) {
+		debug("drop event over...");
+	};
+
+	var handleDrppableOut = function(event, ui) {
+		debug("drop event out...");
 	};
 
 	$.fn.awesomeTree = function(options) {
 		
 		//extend the default options for awesome tree
-		options = $.extend({}, $.awesomeTree.defaults, options);
-
+		var opts = $.extend({}, settings, options);
+		
 		return this.each(function() {
-			var context = $(this);
-			$("li", context).draggable();
+			var $this = $(this);
+
+			//bind drag and drop event for the element
+			$("li", $this).draggable({
+				addClasses: false,
+				helper: "clone",
+				appendTo: "body",
+				opacity: 0.2,
+				delay: 10,
+				cursorAt: settings.cursorAt,
+				start: handleDraggableStart,
+				stop: handleDraggableStop
+			}).droppable({
+				addClasses: false,
+				greedy: false,
+				tolerance: "pointer",
+				drop: handleDroppableDrop,
+				over: handleDroppableOver,
+				out: handleDrppableOut
+			});
+
+			//bind option parmeters to context
+			$this.data("options", opts);
 		});
 	};
 }(jQuery));
