483abd81d3c016084c900d4265d6c78870ccf06b
Shuai Zhang <zhang_s@worksap.co.jp>
2013-09-13 09:48:47 +0800

    define awesome tree

--- src/awesome-tree.js
+++ src/awesome-tree.js
@@ -1,6 +1,23 @@
 (function($) {
+	"use strict";
+
+	$.awesomeTree = {
+
+		//the default parameter option for awesome tree.
+		defaults: {
+
+		};
+
+	};
 
 	$.fn.awesomeTree = function(options) {
-		alert("......");
+		
+		//extend the default options for awesome tree
+		options = $.extend({}, $.awesomeTree.defaults, options);
+
+		return this.each(function() {
+			var context = $(this);
+			$("li", context).draggable();
+		});
 	};
 }(jQuery));
