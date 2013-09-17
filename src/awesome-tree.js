36bcf1ac8098baa6da0707eb64157aba75284878
Shuai Zhang <zhang_s@worksap.co.jp>
2013-09-17 18:51:51 +0800

    refactor the code

--- src/awesome-tree.js
+++ src/awesome-tree.js
@@ -1,10 +1,14 @@
 (function($) {
 	"use strict";
 
-	//the default settings for awesome tree.
-	var settings = {
+	//the default defaults for awesome tree.
+	var defaults = {
 		cursorAt: {left: 0, top: 0},
-		selectedClass: "tree_moving"
+		selectedClass: "node-selected",
+		threshhold: 25,
+		marker: $("<div></div>"),
+		beforeClass: "tdd-before", 
+		afterClass: "tdd-after"
 	};
 
 	var debug = function(obj) {
@@ -23,9 +27,16 @@
 
 	var handleDraggableStart = function(e, ui) {
 		debug("drag event start...");
-		var target = $(e.target);
-		target.addClass(getOptions(target).selectedClass);
+		var target = $(e.target),
+			options = getOptions(target);
+			
+		target.addClass(options.selectedClass);
+		// $(ui.helper).addClass("ui-draggable-helper");
 		$("body").css("cursor", "default");
+	};
+
+	var handleDraggableDrag = function(e, ui) {
+		debug("draggable drag event ...");
 	};
 
 	var handleDraggableStop = function(e, ui) {
@@ -38,17 +49,46 @@
 
 	var handleDroppableOver = function(e, ui) {
 		debug("drop event over...");
+		var options = getOptions($(e.target)),
+			selectedClass = options.selectedClass,
+			beforeClass = options.beforeClass,
+			afterClass = options.afterClass,
+			marker = options.marker;		
+
 		if($(e.target).is("li")) {
-			debug("this is a li element......");
-			debug($(e.target)[0].innerText);
 			$(e.target).bind("mousemove", function(mme) {
 				var target = $(mme.target),
 					x = mme.pageX - target.offset().left,
-					y = mme.pageY - target.offset().top;
-					debug("mouse position: x = " + x + ", y = " + y + ">>>>>>");
+					y = mme.pageY - target.offset().top,
+					threshhold = options.threshhold;
+
+				if(target.find("ul").length !== 0) {
+					threshhold = Math.min(options.threshhold * (target.find("ul").length + 1), target.width() * 0.75);
+				}
+
+				if(target.hasClass(selectedClass) || target.parents("." + selectedClass).length !== 0) {
+					marker.detach();
+				}else {
+
+					// debug("x -- > " + x + ", threshhold -- > " + threshhold);
+					if(x > threshhold) {
+						debug("x > threshhold");
+						if (target.is("li") && target.children("ul").length !== 0) {
+							target.children("ul").append(marker);
+						} else if (target.is("li")) {
+							target.append(marker);
+						}
+					} else if(y < target.height() / 2) {
+						debug("y < height / 2");
+						marker.addClass(beforeClass);
+						target.before(marker);
+					} else {
+						debug("x < threshhold || y > height / 2");
+						marker.addClass(afterClass);
+						target.after(marker);
+					}
+				}
 			});
-		}else {
-			debug("this is not a li element");
 		}
 	};
 
@@ -59,20 +99,21 @@
 	$.fn.awesomeTree = function(options) {
 		
 		//extend the default options for awesome tree
-		var opts = $.extend({}, settings, options);
+		var opts = $.extend({}, defaults, options);
 		
 		return this.each(function() {
 			var $this = $(this);
 
 			//bind drag and drop event for the element
 			$("li", $this).draggable({
-				addClasses: true,
+				addClasses: false,
 				helper: "clone",
 				appendTo: "body",
 				opacity: 0.8,
 				delay: 10,
-				cursorAt: settings.cursorAt,
+				cursorAt: defaults.cursorAt,
 				start: handleDraggableStart,
+				drag: handleDraggableDrag,
 				stop: handleDraggableStop
 			}).droppable({
 				addClasses: false,
